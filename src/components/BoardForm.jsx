93acaf9405ac2e24c43aa86648fe6c8ca2e03986
Ahmed Hussain <ahmedhussainnow@gmail.com>
2025-08-27 19:52:58 +0600

    Boardform created

---
+++ src/components/BoardForm.jsx
@@ -0,0 +1,34 @@
+import { useContext, useRef, useState } from "react";
+import { BoardContext } from "../contexts/Board";
+
+function BoardForm() {
+  const [boardTitle, setBoardTitle] = useState("");
+  const ref = useRef(null);
+  const { boardDispatch } = useContext(BoardContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(ref);
+    
+    if (boardTitle.trim() === "") return alert("Please provide a valid title");
+    boardDispatch({ type: "CREATE_BOARD", payload: boardTitle });
+    setBoardTitle("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={ref}
+          type="text"
+          value={boardTitle}
+          onChange={(e) => {
+            setBoardTitle(e.target.value);
+          }}
+        />
+        <button type="submit">Create Board</button>
+      </form>
+    </div>
+  );
+}
+
+export default BoardForm;
